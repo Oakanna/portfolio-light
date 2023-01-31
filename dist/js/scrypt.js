478908e2250f97782e928e3d37a3451955adf7d0
Anna <example@gmail.com>
2023-01-31 10:25:11 +0300

    added humburger

---
+++ dist/js/scrypt.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return (
+      navigator.userAgent.match(/IEMobile/i) ||
+      navigator.userAgent.match(/WPDesktop/i)
+    );
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
+};
+
+if (isMobile.any()) {
+  document.body.classList.add("_touch");
+} else {
+  document.body.classList.add("_pc");
+}
+// меню бургер
+const menuHumburger = document.querySelector(".menu__humburger");
+const menuBody = document.querySelector(".menu__body");
+if (menuHumburger) {
+  menuHumburger.addEventListener("click", function () {
+    menuHumburger.classList.toggle("active");
+    menuBody.classList.toggle("active");
+  });
+}
+const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
+
+if (menuLinks.length > 0) {
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", onMenuLinkClick);
+  });
+}
+
+function onMenuLinkClick(e) {
+  const menuLink = e.target;
+  if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+    const gotoBlock = document.querySelector(menuLink.dataset.goto);
+    const gotoBlockValue =
+      gotoBlock.getBoundingClientRect().top +
+      pageYOffset -
+      document.querySelector(".menu").offsetHeight;
+
+    if (menuHumburger.classList.contains("active")) {
+      menuHumburger.classList.remove("active");
+      menuBody.classList.remove("active");
+    }
+    window.scrollTo({
+      top: gotoBlockValue,
+      behavior: "smooth",
+    });
+    e.preventDefault();
+  }
+}
