c1c1969eea62dc8ed21413755372f81db43c888b
Anna <example@gmail.com>
2023-02-09 16:13:29 +0300

    dev scill`s star-rating

--- dist/js/scrypt.js
+++ dist/js/scrypt.js
@@ -156,4 +156,42 @@ document.addEventListener("DOMContentLoaded", () => {
   function emailTest(input) {
     return !/^\w+([\.-]?\w+)*@\w+([\.\w{2,8}])+$/.test(input.value);
   }
+
+  const ratings = document.querySelectorAll(".rating");
+  if (ratings.length > 0) {
+    initRatings();
+  }
+
+  // Основная функция
+  function initRatings() {
+    let ratingActive, ratingValue;
+    //   бегаем по всем рейтингам на странице
+    for (let index = 0; index < ratings.length; index++) {
+      const rating = ratings[index];
+      initRating(rating);
+    }
+
+    // Инициализируем конкретный рейтинг
+    function initRating(rating) {
+      initRatingVars(rating);
+
+      setRatingActiveWidth();
+
+      if (rating.classList.contains("rating_set")) {
+        setRating(rating);
+      }
+    }
+
+    // Инициализация переменных
+    function initRatingVars(rating) {
+      ratingActive = rating.querySelector(".rating__active");
+      ratingValue = rating.querySelector(".rating__value");
+    }
+
+    //   Изменяем ширину фктивных звезд
+    function setRatingActiveWidth(index = ratingValue.innerHTML) {
+      const setRatingActiveWidth = index / 0.05;
+      ratingActive.style.width = `${setRatingActiveWidth}%`;
+    }
+  }
 });
